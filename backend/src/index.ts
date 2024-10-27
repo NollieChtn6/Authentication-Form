f570a932ee6e5644ea241ce351d10ff9f4040ad7
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-27 17:36:23 +0100

    create basic server config with Express and Typescript

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+// Useful resources:
+// https://blog.treblle.com/egergr/
+// https://medium.com/deno-the-complete-reference/node-js-8-best-practices-for-express-middlewares-bb5825ec0844
+
+import Express from "express";
+import "reflect-metadata";
+import cors from "cors";
+
+const app = Express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(Express.json());
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+app.listen(PORT, async () => {
+	console.log(`App listening on: http://localhost:${PORT}`);
+});
