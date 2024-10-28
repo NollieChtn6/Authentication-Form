de0ab4b224c5c9d62bb5ef0d40f2b44f7eace823
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-28 22:52:44 +0100

    create route '/users/:id' with GET method and use authenticateJWT middleware to protect this route

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,5 +1,7 @@
 export const userRouter = require("express").Router();
-import { signUp, signIn } from "../controllers/userControllers";
+import { authenticateJWT } from "../middlewares/authMiddleware";
+import { getUserProfile, signUp, signIn } from "../controllers/userControllers";
 
+userRouter.get("/users/:id", authenticateJWT, getUserProfile);
 userRouter.post("/signup", signUp);
 userRouter.post("/login", signIn);
