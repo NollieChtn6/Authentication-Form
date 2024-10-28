1545f45478d1f6e23326edbbd9ff26103dc47426
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-28 22:04:09 +0100

    create loginUserSchema to validate user input on login

--- backend/src/schemas/userSchemas.ts
+++ backend/src/schemas/userSchemas.ts
@@ -20,3 +20,21 @@ export const registerUserSchema = z.object({
 });
 
 export type RegisterUserData = z.infer<typeof registerUserSchema>;
+
+export const loginUserSchema = z.object({
+	email: z
+		.string()
+		.min(1, { message: "email is required" })
+		.regex(/^[a-zA-Z0-9_.±]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/, {
+			message: "email must be a valid adress email",
+		}),
+	password: z
+		.string()
+		.min(1, { message: "password is required" })
+		.regex(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{12,}$/, {
+			message:
+				"password must contain one digit from 1 to 9, one lowercase letter, one uppercase letter, one special character, no space, and it must be at least 12 characters long.",
+		}),
+});
+
+export type LoginUserData = z.infer<typeof loginUserSchema>;
