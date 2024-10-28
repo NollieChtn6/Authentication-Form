30aa54ee0733c34722d4f495e135071899ffab80
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-28 22:02:06 +0100

    use dotenv variables

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,9 +4,10 @@ import cors from "cors";
 import { initializeDataSource } from "./config/db";
 import { catchValidationErrors } from "./middlewares/inputValidation";
 import { userRouter } from "./routes/userRouter";
+require("dotenv").config();
 
 const app = Express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(Express.json());
