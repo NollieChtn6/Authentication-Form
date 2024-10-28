a5ae1930944baa4aff2e70d88edc8e88333c223a
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-28 22:08:06 +0100

    create signIn controller and use loginUser service to allow user login in

--- backend/src/controllers/userControllers.ts
+++ backend/src/controllers/userControllers.ts
@@ -1,8 +1,9 @@
 import type { Request, Response } from "express";
-import { registerUser } from "../services/userServices";
-import { registerUserSchema } from "../schemas/userSchemas";
+import { registerUser, loginUser } from "../services/userServices";
+import { registerUserSchema, loginUserSchema } from "../schemas/userSchemas";
 
 import { StatusCodes } from "http-status-codes";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req: Request, res: Response) => {
 	try {
@@ -16,6 +17,33 @@ export const signUp = async (req: Request, res: Response) => {
 		return res
 			.status(StatusCodes.CREATED)
 			.json({ message: "User has been created", newUser });
+	} catch (error) {
+		return res
+			.status(StatusCodes.INTERNAL_SERVER_ERROR)
+			.json({ error: "Unexpected error occurred" });
+	}
+};
+
+export const signIn = async (req: Request, res: Response) => {
+	try {
+		const { email, password } = loginUserSchema.parse(req.body);
+
+		const user = await loginUser({ email, password });
+		if (!user) {
+			return res
+				.status(StatusCodes.UNAUTHORIZED)
+				.json({ error: "Invalid email or password" });
+		}
+
+		const token = jwt.sign(
+			{ userId: user.id },
+			process.env.JWT_SECRET as string,
+			{ expiresIn: "1h" },
+		);
+
+		return res
+			.status(StatusCodes.OK)
+			.json({ message: "Login successful", token });
 	} catch (error) {
 		return res
 			.status(StatusCodes.INTERNAL_SERVER_ERROR)
