5c40b092be1c97ffe3facecd5e13c6c790aa0a1a
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-28 21:46:59 +0100

    use StatusCodes from http-status-codes

--- backend/src/controllers/userControllers.ts
+++ backend/src/controllers/userControllers.ts
@@ -2,11 +2,23 @@ import type { Request, Response } from "express";
 import { registerUser } from "../services/userServices";
 import { registerUserSchema } from "../schemas/userSchemas";
 
+import { StatusCodes } from "http-status-codes";
+
 export const signUp = async (req: Request, res: Response) => {
-	const { firstName, email, password } = registerUserSchema.parse(req.body);
-	const newUser = await registerUser({ firstName, email, password });
-	if (!newUser) {
-		return res.status(409).json({ error: "User already exists in database" });
+	try {
+		const { firstName, email, password } = registerUserSchema.parse(req.body);
+		const newUser = await registerUser({ firstName, email, password });
+		if (!newUser) {
+			return res
+				.status(StatusCodes.CONFLICT)
+				.json({ error: "User already exists in database" });
+		}
+		return res
+			.status(StatusCodes.CREATED)
+			.json({ message: "User has been created", newUser });
+	} catch (error) {
+		return res
+			.status(StatusCodes.INTERNAL_SERVER_ERROR)
+			.json({ error: "Unexpected error occurred" });
 	}
-	return res.status(201).json({ message: "User has been created", newUser });
 };
