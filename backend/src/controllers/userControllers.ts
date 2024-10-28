31b143b3759346aab091a0acc2b0eec5560ff65e
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-10-28 22:50:50 +0100

    create getUserProfile and use getUserById service to fetch data from database

--- backend/src/controllers/userControllers.ts
+++ backend/src/controllers/userControllers.ts
@@ -1,9 +1,26 @@
 import type { Request, Response } from "express";
-import { registerUser, loginUser } from "../services/userServices";
+import { getUserById, registerUser, loginUser } from "../services/userServices";
 import { registerUserSchema, loginUserSchema } from "../schemas/userSchemas";
 
 import { StatusCodes } from "http-status-codes";
 import jwt from "jsonwebtoken";
+
+export const getUserProfile = async (req: Request, res: Response) => {
+	try {
+		const userId = Number(req.params.id);
+		const user = await getUserById(userId);
+		if (!user) {
+			return res
+				.status(StatusCodes.NOT_FOUND)
+				.json({ error: "User not found" });
+		}
+		return res.status(StatusCodes.OK).json(user);
+	} catch (error) {
+		return res
+			.status(StatusCodes.INTERNAL_SERVER_ERROR)
+			.json({ error: "Unexpected error occurred" });
+	}
+};
 
 export const signUp = async (req: Request, res: Response) => {
 	try {
