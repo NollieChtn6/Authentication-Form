9c299abd4c7cd205069dcec9cfed9a1e79be5dbb
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-01 22:44:50 +0100

    update route '/users/:id' to '/:id' to avoid repeating 'users' as userRouter is now used as '/users' in global router

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -2,6 +2,6 @@ export const userRouter = require("express").Router();
 import { authenticateJWT } from "../middlewares/authMiddleware";
 import { getUserProfile, signUp, signIn } from "../controllers/userControllers";
 
-userRouter.get("/users/:id", authenticateJWT, getUserProfile);
+userRouter.get("/:id", authenticateJWT, getUserProfile);
 userRouter.post("/signup", signUp);
 userRouter.post("/login", signIn);
