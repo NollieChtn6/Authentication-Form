457911a6268bf9bc76b785937facd848a26f53ee
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-01 23:23:17 +0100

    fix: setting userId as id to later decode token using userId

--- backend/src/utils/generateJWT.ts
+++ backend/src/utils/generateJWT.ts
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
 
 export const generateJWT = (userId: number, secret: string): string => {
-	return jwt.sign({ userId }, secret, { expiresIn: "1h" });
+	return jwt.sign({ id: userId }, secret, { expiresIn: "1h" });
 };
