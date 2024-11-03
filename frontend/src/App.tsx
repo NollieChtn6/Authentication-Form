5e8fc402a494ed5115ec632a6542e9df8741ace7
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-03 17:34:33 +0100

    import router and configure matching components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,23 @@
+import { Router } from "./router/router";
+import { match } from "ts-pattern";
+
+import { Home } from "./pages/Home";
+import { LoginForm } from "./components/LoginForm";
+import { SignUpForm } from "./components/SignupForm";
+
 import "./App.css";
 
 function App() {
+	const route = Router.useRoute(["Home", "Login", "Signup"]);
 	return (
 		<>
-			<div>Hello World</div>
+			{match(route)
+				.with({ name: "Home" }, () => <Home />)
+				.with({ name: "Login" }, () => <LoginForm />)
+				.with({ name: "Signup" }, () => <SignUpForm />)
+				.otherwise(() => (
+					<h1 className="">Oops! Not found!</h1>
+				))}
 		</>
 	);
 }
