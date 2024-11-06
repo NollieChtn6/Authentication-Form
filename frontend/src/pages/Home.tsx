3b205fee2334b64ee140e02bb464da010ecc839c
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-06 07:27:09 +0100

    create buttons to redirect to login and signup forms

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,19 @@
+import { Link } from "@swan-io/chicane";
+import { Router } from "../router/router";
+
 export function Home() {
-	return <>Homepage</>;
+	return (
+		<div className="home-panel">
+			<Link to={Router.Signup()}>
+				<button className="btn" type="button">
+					I want to sign in!
+				</button>
+			</Link>
+			<Link to={Router.Login()}>
+				<button className="btn" type="button">
+					I want to log in!
+				</button>
+			</Link>
+		</div>
+	);
 }
