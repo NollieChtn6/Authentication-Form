706377c246ce278cd5728d79f114601420785027
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-06 07:27:31 +0100

    create structure of LoginForm

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,3 +1,26 @@
 export function LoginForm() {
-	return <>Login!</>;
+	return (
+		<form className="form login-form">
+			<div className="form-item">
+				<label htmlFor="email">Your Email</label>
+				<input
+					type="email"
+					id="email"
+					name="email"
+					placeholder="email@example.com"
+				/>
+				<p className="validation-error email-error" />
+			</div>
+			<div className="form-item">
+				<label htmlFor="password">Your Password</label>
+				<input type="password" id="password" name="password" />
+				<p className="validation-error password-error" />
+			</div>
+			<div className="btn-container">
+				<button className="btn" type="submit">
+					Log In
+				</button>
+			</div>
+		</form>
+	);
 }
