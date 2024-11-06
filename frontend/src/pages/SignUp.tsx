c536b7f7ce3935d8b28a46ffddcab1d600e4f6c8
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-06 07:26:30 +0100

    create SignUp  page structure and import SignUpForm

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,3 +1,20 @@
+import { Link } from "@swan-io/chicane";
+import { Router } from "../router/router";
+import { SignUpForm } from "../components/SignupForm";
+
 export function SignUp() {
-	return <div>Signup!</div>;
+	return (
+		<div className="page-container">
+			<h2>Sign Up</h2>
+			<SignUpForm />
+			<p>OR</p>
+			<div>Sign up with Google</div>
+			<p className="option auth-option">
+				Already have an account?{" "}
+				<span className="link">
+					<Link to={Router.Login()}>Login!</Link>
+				</span>
+			</p>
+		</div>
+	);
 }
