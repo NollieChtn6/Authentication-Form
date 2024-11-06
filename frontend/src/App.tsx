36ebbf020b3acea8280000be74b4882adbca3117
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-06 07:29:29 +0100

    match routes with pages instead of components and create footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,32 @@
-import { Router } from "./router/router";
 import { match } from "ts-pattern";
+import { Router } from "./router/router";
 
 import { Home } from "./pages/Home";
-import { LoginForm } from "./components/LoginForm";
-import { SignUpForm } from "./components/SignupForm";
+import { LogIn } from "./pages/Login";
+import { SignUp } from "./pages/SignUp";
 
 import "./App.css";
 
 function App() {
 	const route = Router.useRoute(["Home", "Login", "Signup"]);
 	return (
-		<>
+		<main>
 			{match(route)
 				.with({ name: "Home" }, () => <Home />)
-				.with({ name: "Login" }, () => <LoginForm />)
-				.with({ name: "Signup" }, () => <SignUpForm />)
+				.with({ name: "Login" }, () => <LogIn />)
+				.with({ name: "Signup" }, () => <SignUp />)
 				.otherwise(() => (
 					<h1 className="">Oops! Not found!</h1>
 				))}
-		</>
+			<footer>
+				<p>
+					Crafted with 💙 by{" "}
+					<span>
+						<a href="https://github.com/nolliechtn6">NollieChtn6</a>
+					</span>
+				</p>
+			</footer>
+		</main>
 	);
 }
 
