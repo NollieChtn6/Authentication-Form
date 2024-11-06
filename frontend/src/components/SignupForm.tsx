eee5f5285bdbb4cd151984cc97c31eaa7b461b60
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-06 07:28:02 +0100

    create structure of Signup Form

--- frontend/src/components/SignupForm.tsx
+++ frontend/src/components/SignupForm.tsx
@@ -1,3 +1,43 @@
 export function SignUpForm() {
-	return <>Signup!</>;
+	return (
+		<form className="form signup-form">
+			<div className="form-item">
+				<label htmlFor="firstName">Your Name</label>
+				<input type="text" id="firstName" name="firstName" placeholder="John" />
+				<p className="validation-error firstName-error" />
+			</div>
+			<div className="form-item">
+				<label htmlFor="email">Your Email</label>
+				<input
+					type="email"
+					id="email"
+					name="email"
+					placeholder="email@example.com"
+				/>
+				<p className="validation-error email-error" />
+			</div>
+			<div className="form-item">
+				<label htmlFor="password">Your Password</label>
+				<input type="password" id="password" name="password" />
+				<p className="validation-error password-error" />
+			</div>
+			<div className="form-item">
+				<label htmlFor="confirmPassword">Confirm Password</label>
+				<input type="password" id="password" name="password" />
+				<p className="validation-error confirmPssword-error" />
+			</div>
+			<div className="form-item">
+				<div className="checkbox-container">
+					<input type="checkbox" id="password" name="password" />
+					<label htmlFor="password">I agree and I want to sign in!</label>
+				</div>
+				<p className="validation-error password-error" />
+			</div>
+			<div className="btn-container">
+				<button className="btn" type="submit">
+					Sign Up
+				</button>
+			</div>
+		</form>
+	);
 }
