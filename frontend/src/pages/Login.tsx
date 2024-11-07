820c7e41a4abc0587c46fdebb600382ff968cf42
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-07 21:29:26 +0100

    create 'Sign in with Google' button

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,13 +2,26 @@ import { Link } from "@swan-io/chicane";
 import { Router } from "../router/router";
 import { LoginForm } from "../components/LoginForm";
 
+import { GoogleLogin } from "@react-oauth/google";
+
 export function LogIn() {
 	return (
 		<div className="page-container">
 			<h2>Login</h2>
 			<LoginForm />
 			<p>OR</p>
-			<div>Log In with Google</div>
+			<GoogleLogin
+				onSuccess={(credentialResponse) => {
+					console.log(credentialResponse);
+				}}
+				onError={() => {
+					console.log("Login Failed");
+				}}
+				auto_select={true}
+				theme="filled_blue"
+				context="signin"
+				locale="en"
+			/>
 			<p className="option auth-option">
 				No account yet?{" "}
 				<span className="link">
