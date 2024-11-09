1fdebe727b250f4b3f7794b80a48bc9e0cbf18f3
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-09 20:46:40 +0100

    create route '/logout' with POST method

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,7 +1,8 @@
 export const userRouter = require("express").Router();
 import { authenticateJWT } from "../middlewares/authMiddleware";
-import { getUserProfile, signUp, signIn } from "../controllers/userControllers";
+import { getUserProfile, signUp, signIn, logout } from "../controllers/userControllers";
 
 userRouter.get("/:id", authenticateJWT, getUserProfile);
 userRouter.post("/signup", signUp);
 userRouter.post("/login", signIn);
+userRouter.post("/logout", logout);
