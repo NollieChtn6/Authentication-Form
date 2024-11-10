0d5497cc470b1f961b28aee233eb922f35999a29
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-10 11:01:33 +0100

    move Google button to signup form

--- frontend/src/components/SignupForm.tsx
+++ frontend/src/components/SignupForm.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { Link } from "@swan-io/chicane";
+import { GoogleLogin } from "@react-oauth/google";
 import { Router } from "../router/router";
 import { registerUser } from "../services/userServices";
 
 import type { SignUpFormData, SuccessMessage } from "../@types/data.t";
+
+import { SquareArrowOutUpRight } from "lucide-react";
 
 export function SignUpForm() {
 	const [formData, setFormData] = useState<SignUpFormData>({
@@ -51,83 +54,103 @@ export function SignUpForm() {
 	};
 
 	return (
-		<form className="form signup-form" onSubmit={handleFormSubmit}>
+		<div className="form-container">
 			{successMessage ? (
-				<div>
+				<div className="success-message-container">
 					<p>{successMessage}</p>
 					<Link to={Router.Login()}>
-						<button type="button">Go to Login</button>
+						<p className="cta-link">
+							Go to Login!{" "}
+							<span>
+								<SquareArrowOutUpRight />
+							</span>
+						</p>
 					</Link>
 				</div>
 			) : (
 				<>
-					<div className="form-item">
-						<label htmlFor="firstName">Your Name</label>
-						<input
-							type="text"
-							id="firstName"
-							name="firstName"
-							value={formData.firstName}
-							onChange={handleFieldChange}
-							placeholder="John"
-						/>
-					</div>
-					<div className="form-item">
-						<label htmlFor="email">Your Email</label>
-						<input
-							type="email"
-							id="email"
-							name="email"
-							value={formData.email}
-							onChange={handleFieldChange}
-							placeholder="email@example.com"
-						/>
-					</div>
-					<div className="form-item">
-						<label htmlFor="password">Your Password</label>
-						<input
-							type="password"
-							id="password"
-							name="password"
-							value={formData.password}
-							onChange={handleFieldChange}
-						/>
-					</div>
-					<div className="form-item">
-						<label htmlFor="confirmPassword">Confirm Password</label>
-						<input
-							type="password"
-							id="confirmPassword"
-							name="confirmPassword"
-							value={formData.confirmPassword}
-							onChange={handleFieldChange}
-						/>
-					</div>
-					<div className="form-item">
-						<div className="checkbox-container">
+					<form className="form signup-form" onSubmit={handleFormSubmit}>
+						<div className="form-item">
+							<label htmlFor="firstName">Your Name</label>
 							<input
-								type="checkbox"
-								id="agreeToTerms"
-								name="agreeToTerms"
-								checked={formData.agreeToTerms}
+								type="text"
+								id="firstName"
+								name="firstName"
+								value={formData.firstName}
+								onChange={handleFieldChange}
+								placeholder="John"
+							/>
+						</div>
+						<div className="form-item">
+							<label htmlFor="email">Your Email</label>
+							<input
+								type="email"
+								id="email"
+								name="email"
+								value={formData.email}
+								onChange={handleFieldChange}
+								placeholder="email@example.com"
+							/>
+						</div>
+						<div className="form-item">
+							<label htmlFor="password">Your Password</label>
+							<input
+								type="password"
+								id="password"
+								name="password"
+								value={formData.password}
 								onChange={handleFieldChange}
 							/>
-							<label htmlFor="agreeToTerms">
-								I agree and I want to sign up!
-							</label>
 						</div>
-					</div>
-					<div className="btn-container">
-						<button
-							className="btn"
-							type="submit"
-							disabled={!formData.agreeToTerms}
-						>
-							Sign Up
-						</button>
-					</div>
+						<div className="form-item">
+							<label htmlFor="confirmPassword">Confirm Password</label>
+							<input
+								type="password"
+								id="confirmPassword"
+								name="confirmPassword"
+								value={formData.confirmPassword}
+								onChange={handleFieldChange}
+							/>
+						</div>
+						<div className="form-item">
+							<div className="checkbox-container">
+								<input
+									type="checkbox"
+									id="agreeToTerms"
+									name="agreeToTerms"
+									checked={formData.agreeToTerms}
+									onChange={handleFieldChange}
+								/>
+								<label htmlFor="agreeToTerms">
+									I agree and I want to sign up!
+								</label>
+							</div>
+						</div>
+						<div className="btn-container">
+							<button
+								className="btn"
+								type="submit"
+								disabled={!formData.agreeToTerms}
+							>
+								Sign Up
+							</button>
+						</div>
+					</form>
+					<p>OR</p>
+					<GoogleLogin
+						onSuccess={(credentialResponse) => {
+							console.log(credentialResponse);
+						}}
+						onError={() => {
+							console.log("Login Failed");
+						}}
+						auto_select={true}
+						theme="filled_blue"
+						text="signup_with"
+						locale="en"
+					/>
 				</>
 			)}
-		</form>
+		</div>
 	);
 }
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,5 +1,4 @@
 import { Link } from "@swan-io/chicane";
-import { GoogleLogin } from "@react-oauth/google";
 import { Router } from "../router/router";
 import { SignUpForm } from "../components/SignupForm";
 
@@ -8,19 +7,6 @@ export function SignUp() {
 		<div className="page-container">
 			<h2>Sign Up</h2>
 			<SignUpForm />
-			<p>OR</p>
-			<GoogleLogin
-				onSuccess={(credentialResponse) => {
-					console.log(credentialResponse);
-				}}
-				onError={() => {
-					console.log("Login Failed");
-				}}
-				auto_select={true}
-				theme="filled_blue"
-				text="signup_with"
-				locale="en"
-			/>
 			<p className="option auth-option">
 				Already have an account?{" "}
 				<span className="link">
